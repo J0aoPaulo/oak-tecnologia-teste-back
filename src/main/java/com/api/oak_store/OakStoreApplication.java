67338ab3013da3170bd49c8aff3a9aafe26fde14
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-01-02 19:05:53 -0300

    feat: setup project

---
+++ src/main/java/com/api/oak_store/OakStoreApplication.java
@@ -0,0 +1,13 @@
+package com.api.oak_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakStoreApplication.class, args);
+	}
+
+}
